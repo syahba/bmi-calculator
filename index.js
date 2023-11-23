d52931304dee3d471e74868d1d820a080af663b4
syahba <ksyaahba@gmail.com>
2023-11-23 08:06:38 +0700

    complete working bmi calculator

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+// call elements
+const submit = document.getElementById('submit');
+const results = document.getElementById('results');
+const inputBox = document.getElementsByClassName('input-box');
+const card = document.getElementsByClassName('card');
+
+// start action
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    getInput();
+});
+
+// get input value
+const getInput = () => {
+    const weight = document.getElementById('weight').value;
+    const height = document.getElementById('height').value;
+
+    inputBox[0].style.borderColor = '#C9E0E5';
+    inputBox[1].style.borderColor = '#C9E0E5';
+
+    if (!weight || !height) {
+        if (!weight) {
+            inputBox[0].style.borderColor = 'red';
+        };
+        if (!height) {
+            inputBox[1].style.borderColor = 'red';
+        };
+        results.innerHTML = `<p style="color:red">Please check your weight and height again</p>`;
+    } else {
+        getBMI(weight, height);
+        card[0].style.height = '528px';
+    };
+};
+
 // calculate bmi
 const getBMI = (weight, height) => {
     let category;
@@ -7,7 +41,5 @@ const getBMI = (weight, height) => {
     bmi < 18.5 ? category = 'Underweight' : bmi >= 18.5 && bmi <= 24.9 ? category = 'Normal' 
     : bmi >= 25 && bmi <= 29.9 ? category = 'Overweight' : category = 'Obesity';
 
-    const res = `Your BMI is ${bmi} which means you are ${category}`;
-    return res;
+    results.innerHTML = `<p>Your BMI is <b>${bmi}</b> which means you are <b>${category}</b></p>`;
 };
-console.log(getBMI(40, 155));
